f9db44bfb02d094f75239520ff603df0150aa995
michaeldel <michaeldel@protonmail.com>
2020-11-19 10:03:17 +0100

    Make program list available modules

--- nupydoc/__main__.py
+++ nupydoc/__main__.py
@@ -1,2 +1,9 @@
-print("Hello world!")
+import pkgutil
+
+for module in pkgutil.iter_modules():
+    # ignore modules marked private by default
+    if module.name.startswith('_'):
+        continue
+
+    print(module.name)
 
