8a0745e6c199105a95c5cf19c2998b1dd0a30a45
michaeldel <michaeldel@protonmail.com>
2020-11-19 10:12:40 +0100

    Temporarily stop listing standard modules

--- nupydoc/__main__.py
+++ nupydoc/__main__.py
@@ -1,8 +1,15 @@
+import importlib
 import pkgutil
+
+
+def is_standard_library(module: pkgutil.ModuleInfo) -> bool:  # TODO: find better heuristic
+    return 'site-packages' not in module.module_finder.path
 
 for module in pkgutil.iter_modules():
     # ignore modules marked private by default
     if module.name.startswith('_'):
+        continue
+    if hasattr(module.module_finder, 'path') and is_standard_library(module):
         continue
 
     print(module.name)
