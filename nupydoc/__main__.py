9200f8548a89226605a5f8631b49755c69455239
michaeldel <michaeldel@protonmail.com>
2020-11-20 05:54:26 +0100

    Walk modules to be processed by inspection

--- nupydoc/__main__.py
+++ nupydoc/__main__.py
@@ -23,11 +23,19 @@ def display_tree(tree: ModuleTree, prefix=""):
         display_tree(submodule, prefix=f"  {prefix}")
 
 
+def walk_modules(tree: ModuleTree, package: str = ''):
+    prefix = '.' if package else ''
+    module = importlib.import_module(f'{prefix}{tree.name}', package=package)
+    print(module.__name__)
+    for submodule in tree.submodules:
+        walk_modules(submodule, module.__name__)
+
+
 def is_standard_library(module: pkgutil.ModuleInfo) -> bool:  # TODO: find better heuristic
     return 'site-packages' not in module.module_finder.path
 
 
-def walk_modules(path: Optional[str] = None) -> Iterable[ModuleTree]:
+def compute_module_tree(path: Optional[str] = None) -> Iterable[ModuleTree]:
     for module in pkgutil.iter_modules([path] if path is not None else None):
         # ignore modules marked private by default
         if module.name.startswith('_'):
@@ -39,11 +47,11 @@ def walk_modules(path: Optional[str] = None) -> Iterable[ModuleTree]:
 
         if module.ispkg:
             subpath = os.path.join(module.module_finder.path, module.name)
-            tree.submodules.extend(walk_modules(subpath))
+            tree.submodules.extend(compute_module_tree(subpath))
 
         yield tree
 
 
-for tree in walk_modules():
-    display_tree(tree)
+for tree in compute_module_tree():
+    walk_modules(tree)
 
